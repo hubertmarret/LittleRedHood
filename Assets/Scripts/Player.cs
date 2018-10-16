49f37bd0ca8843655e770a50e0bf6ea3aaf3af07
hubertmarret <hubert.marret@gmail.com>
2018-10-16 18:25:59 +0200

    HubScene in the dark with trees

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+ 
+
+    void Move()
+    {
+        if(Input.GetKey("z"))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
+    }
+
+
+	void Update () {
+
+        Move();
+    }
+}
