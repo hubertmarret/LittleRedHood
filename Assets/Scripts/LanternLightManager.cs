08da21ab6c0bb3e919e1bed097127de8f4a61e37
Vovoda <vovoda@orange.fr>
2018-10-16 18:52:21 +0200

    First implementation of light management

---
+++ Assets/Scripts/LanternLightManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternLightManager : MonoBehaviour {
+
+    public float lightSpeedFactor = 1f;
+    public float lightMax = 20f;
+    public float currentLight = 20f;
+    public new Light light;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentLight = currentLight - lightSpeedFactor * Time.deltaTime;
+        light.range = currentLight;
+	}
+}
