bb0ab4b829c3373cbd35c93304c835f798c811a3
hubertmarret <hubert.marret@gmail.com>
2018-10-18 14:58:07 +0200

    ezfezfezf

--- Assets/Scripts/RessourceSpawner.cs
+++ Assets/Scripts/RessourceSpawner.cs
@@ -6,19 +6,33 @@ public class RessourceSpawner : MonoBehaviour {
 
     public GameObject orbPrefab;
 
+    public float lightPowerToSpawn = 7;
+    public int nbOrbsToSpawn = 1;
+    public float distanceFromPlayer = 20;
+
+    private GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 playerPos = player.transform.position;
+
+        //if(player.GetComponent<LanternLightManager>().currentLight == 7)
 		if(Input.GetKeyUp("space"))
         {
-            float x = Random.Range(-20.0f, 20.0f);
-            float z = Random.Range(-10.0f, 20.0f);
-            GameObject orb = Instantiate(orbPrefab);
-            orb.transform.position = new Vector3(x, orb.transform.position.y, z);
+            for (int i = 0; i < nbOrbsToSpawn; ++i)
+            {
+                int multiplier = (Random.Range(1, 2) * 2) - 3;
+                float randX = Random.Range(distanceFromPlayer - 10, distanceFromPlayer + 10) * multiplier;
+                float randZ = Random.Range(distanceFromPlayer - 10, distanceFromPlayer + 10) * multiplier;
+                
+                GameObject orb = Instantiate(orbPrefab);
+                orb.transform.position = new Vector3(orb.transform.position.x + randX, orb.transform.position.y, orb.transform.position.z + randZ);
+            }
         }
 	}
 }
